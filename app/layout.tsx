be8fb5c34a198be53a0d850c232e33e7dda95df6
tichuu77 <akashtikhat50@gmail.com>
2026-03-25 22:00:18 +0530

    update scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -89,19 +89,34 @@ export default function RootLayout({
   return (
     <html lang="en-IN">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17888150548">
-        </script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){window.dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'AW-17888150548');
-      `,
-          }}
+        {/* Google Ads Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17888150548"
+          strategy="afterInteractive"
         />
+
+        <Script id="google-ads-init" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      window.gtag = gtag;
+
+      gtag('js', new Date());
+      gtag('config', 'AW-17888150548');
+    `}
+        </Script>
+
+        <Script id="google-ads-conversion" strategy="afterInteractive">
+          {`
+    if (window.gtag) {
+      window.gtag('event', 'conversion', {
+        send_to: 'AW-17888150548/QgQDCLCOy4kcEJSI3tFC'
+      });
+    }
+  `}
+        </Script>
       </head>
+
       <body className="font-sans antialiased">
         <Script
           id="local-business-schema"
